055b937b7d2b10543ad251d1bb0f45894de7eaa6
Aleyna Celik <clkkaley@gmail.com>
2022-09-03 14:55:46 +0300

    added array and array methods

---
+++ javascript-temelleri/diziler/index.js
@@ -0,0 +1,18 @@
+let urun1="iphone 12"
+let urun2="iphone 12"
+let urun3="iphone 12"
+
+let urunler=["iphone 12i","iphone 13","iphone 14"];
+let fiyatlar=[9000,2000,3000];
+
+// console.log("ürünler:",urunler);
+// console.log("fiyatlar:",fiyatlar);
+
+
+let ogrenciler=["çınar","ayşe","fatma"]
+console.log("ogrenciler:" ,ogrenciler);
+console.log("ogrencilerin uzunluğu:" ,ogrenciler.length);
+console.log("ogrencilerin toString:" ,ogrenciler.toString());
+console.log("öğrenciler join",ogrenciler.join(" "));
+
+
