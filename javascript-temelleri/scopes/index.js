dd19fff919357b76c770d40525598da2ed3a8e70
Aleyna Celik <clkkaley@gmail.com>
2022-09-04 00:18:17 +0300

    added scopes

---
+++ javascript-temelleri/scopes/index.js
@@ -0,0 +1,30 @@
+var isim="aleyna"
+
+function yaz(){
+    var isim="ayşe"
+    var yas=25;
+    console.log(isim,yas);
+}
+
+yaz();
+//console.log(yas);
+
+
+
+if(true){
+    var isim="ali"
+    console.log(isim);
+}
+console.log(isim);
+
+
+if(true){
+    let isim="mahmut"
+    console.log(isim);
+}
+console.log(isim);
+
+
+const adres="istanbul"
+
+adres="Kocaeli"
