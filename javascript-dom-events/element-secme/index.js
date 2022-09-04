a70a0875baf9eeee5ac9aa963c62e07271d0702e
Aleyna Celik <clkkaley@gmail.com>
2022-09-04 13:41:52 +0300

    choose single element

---
+++ javascript-dom-events/element-secme/index.js
@@ -0,0 +1,7 @@
+let sonuc;
+sonuc=document.getElementById("title").innerText;
+sonuc=document.getElementById("title").className;
+
+document.getElementById("title").style.color="red"
+
+console.log(sonuc);
