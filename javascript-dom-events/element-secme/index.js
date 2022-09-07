c19f67e9a10d78c8dd33680192178285e6492304
Aleyna Celik <clkkaley@gmail.com>
2022-09-07 15:20:50 +0300

    added coklu element seçimi

--- javascript-dom-events/element-secme/index.js
+++ javascript-dom-events/element-secme/index.js
@@ -1,7 +1,23 @@
+// let sonuc;
+// sonuc=document.getElementById("title").innerText;
+// sonuc=document.getElementById("title").className;
+
+// document.getElementById("title").style.color="red"
+
+// console.log(sonuc);
+
+
+
 let sonuc;
-sonuc=document.getElementById("title").innerText;
-sonuc=document.getElementById("title").className;
+sonuc=document.getElementsByClassName('task')[0].innerHTML;
+sonuc=document.getElementsByClassName('task');
 
-document.getElementById("title").style.color="red"
+// for(let i=0;i<=sonuc.length;i++){
+//     console.log(sonuc[i]);
+// }
 
+for(let task of sonuc){
+    task.style.color="red"
+    console.log(sonuc);
+}
 console.log(sonuc);
