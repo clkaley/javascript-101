79cb9e33d690b33aec12ef4e2f98d7bf0a94e394
Aleyna Celik <clkkaley@gmail.com>
2022-09-07 16:37:45 +0300

    added element olusturma

---
+++ javascript-dom-events/element-olusturma/index.js
@@ -0,0 +1,19 @@
+let sonuc;
+let gorevListesi = ["görev 1,", "görev2"];
+ul = document.getElementById("task-list");
+// let count=ul.children.length
+for (let index in gorevListesi) {
+  let li = `  <li class="task">
+<div class="form-check">
+    <input type="checkbox" id="${index + 1}"
+    class="form-check-input">
+    <label for="${index + 1}">Hello ${gorevListesi[index]}</label>
+</div>
+</li>`;
+ul.insertAdjacentHTML("beforeEnd", li);
+}
+// console.log(count);
+
+
+
+
